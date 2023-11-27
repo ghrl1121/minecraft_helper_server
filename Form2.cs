eff55ea0f5f1d14c6f44c84f3bee3b8318027a22
ghrl1121 <70801233+ghrl1121@users.noreply.github.com>
2023-11-27 22:10:02 +0900

    수정

--- Form2.cs
+++ Form2.cs
@@ -63,7 +63,7 @@ namespace 마크서버_만들기
                 }
                 else
                 {
-                    string[] lines = { "@echo off", "java -Xmx" +aq + "M" + " -Xms" + aw + "M" +" -jar "+ AA+ " nogui", "pause"};
+                    string[] lines = { "@echo off","title ", "java -Xmx" +aq + "M" + " -Xms" + aw + "M" +" -jar "+ AA+ " nogui", "pause"};
                     File.WriteAllLines(AB+@"\comms.bat",lines);
                     Process A =new Process();
                     A.StartInfo.FileName = "comms.bat";
@@ -88,7 +88,7 @@ namespace 마크서버_만들기
                 }
                 else
                 {
-                    string[] lines = { "@echo", "java -Xmx" + aq + "G" + " -Xms" + aw+ "G" + " -jar " + aa + " nogui", "pause" };
+                    string[] lines = { "@echo off", "title ", "java -Xmx" + aq + "G" + " -Xms" + aw+ "G" + " -jar " + aa + " nogui", "pause" };
                     File.WriteAllLines(ab + @"\comms.bat",lines);
                     Process A =new Process();
                     A.StartInfo.FileName = "comms.bat";
