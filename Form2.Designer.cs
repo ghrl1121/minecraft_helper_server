c85b71e5a7c63109a67d91156788fa058fb085db
ghrl1121 <ghrl1005@naver.com>
2025-12-24 22:05:42 +0900

    수정

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -38,7 +38,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -104,7 +103,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(238, 21);
+            this.button1.Location = new System.Drawing.Point(241, 43);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 65);
             this.button1.TabIndex = 7;
@@ -129,23 +128,12 @@
             this.label6.TabIndex = 9;
             this.label6.Text = "위치";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(238, 95);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 60);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "포트\r\n포워딩";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(328, 167);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button1);
@@ -175,6 +163,5 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button2;
     }
 }
